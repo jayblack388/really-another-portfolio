cf816115be62a3dd972beb4cdd088c503aa3935a
John Blackwell <jblackwell072588@gmail.com>
2021-06-04 13:49:54 -0700

    add netlify-cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
+});
 module.exports = {
 	/* Your site config here */
 	siteMetadata: {
@@ -14,17 +11,29 @@ module.exports = {
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
-				name: `images`,
-				path: `${__dirname}/src/images/`,
+				name: `blog`,
+				path: `${__dirname}/src/blog/`,
 			},
 		},
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
-				name: `blog`,
-				path: `${__dirname}/src/blog/`,
+				name: `images`,
+				path: `${__dirname}/src/images/`,
+			},
+		},
+		{
+			resolve: 'gatsby-source-graphql',
+			options: {
+				typeName: 'GitHub',
+				fieldName: 'github',
+				url: 'https://api.github.com/graphql',
+				headers: {
+					Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+				},
 			},
 		},
 		`gatsby-transformer-remark`,
+		`gatsby-plugin-netlify-cms`,
 	],
 };
